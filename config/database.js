281bd0ddad8de5c166abc67b0fa8e78557bb9bd2
Sandeep <guptasandy876@gmail.com>
2022-04-21 11:03:59 +0530

    changes in clients

--- config/database.js
+++ config/database.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 dotenv.config({
     path: "../config/.env"
 });
-
 // const DB = "mongodb://localhost:27017/updown";
 const DB = 'mongodb+srv://sandeep:5ezO3gutGKB02WEs@cluster0.2xwjp.mongodb.net/updown?retryWrites=true&w=majority';
 
